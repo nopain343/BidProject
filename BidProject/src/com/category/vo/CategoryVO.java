c036e42c13a43813f6e06084d241255fdf5bee3e
JY <ljiyun118@naver.com>
2018-10-23 15:10:44 +0900

    CategoryVO

--- BidProject/src/com/category/vo/CategoryVO.java
+++ BidProject/src/com/category/vo/CategoryVO.java
@@ -3,77 +3,69 @@ package com.category.vo;
 public class CategoryVO {
 
 	private String cat1;
-	private String name_p;
+	private String prodname;
 	private int price;
+	private String condition;
 	private String ref;
 	private String code;
 	private String id;
 	private String term;
 	private String descrip;
-
 	public String getCat1() {
 		return cat1;
 	}
-
 	public void setCat1(String cat1) {
 		this.cat1 = cat1;
 	}
-
-	public String getName_p() {
-		return name_p;
+	public String getProdname() {
+		return prodname;
 	}
-
-	public void setName_p(String name_p) {
-		this.name_p = name_p;
+	public void setProdname(String prodname) {
+		this.prodname = prodname;
 	}
-
 	public int getPrice() {
 		return price;
 	}
-
 	public void setPrice(int price) {
 		this.price = price;
 	}
-
+	public String getCondition() {
+		return condition;
+	}
+	public void setCondition(String condition) {
+		this.condition = condition;
+	}
 	public String getRef() {
 		return ref;
 	}
-
 	public void setRef(String ref) {
 		this.ref = ref;
 	}
-
 	public String getCode() {
 		return code;
 	}
-
 	public void setCode(String code) {
 		this.code = code;
 	}
-
 	public String getId() {
 		return id;
 	}
-
 	public void setId(String id) {
 		this.id = id;
 	}
-
 	public String getTerm() {
 		return term;
 	}
-
 	public void setTerm(String term) {
 		this.term = term;
 	}
-
 	public String getDescrip() {
 		return descrip;
 	}
-
 	public void setDescrip(String descrip) {
 		this.descrip = descrip;
 	}
-	
+
+
 	
 }
